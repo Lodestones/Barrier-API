0f14955795567c1197dfba72718c0764cbb82b21
John Aquino <41652412+apollyon600@users.noreply.github.com>
2025-04-09 17:20:29 -0700

    feat: IStorm#getCurrentRadius & IStorm#getTargetRadius

--- src/main/java/to/lodestone/barrierapi/IStorm.java
+++ src/main/java/to/lodestone/barrierapi/IStorm.java
@@ -5,26 +5,49 @@ import org.bukkit.entity.Player;
 
 public interface IStorm {
     double getDamage();
+
     int getDamageTick();
+
     double getX();
+
     double getZ();
+
     void setTicksToShrink(int time);
+
     void setRadius(double radius, int time);
+
     void setRadius(double radius);
-    void setTicksToMove(int time);
+
     void setRadius(int radius);
+
+    void setTicksToMove(int time);
+
     void update();
+
     void setDamage(double damage);
-    double getRadius();
+
+    double getCurrentRadius();
+
+    double getTargetRadius();
+
     void setDamageTick(int damageTick);
+
     void setOrigin(double x, double z, int time);
+
     void setOrigin(double x, double z);
+
     void setX(double x);
+
     void setZ(double z);
+
     int getTicksBeforeDamage();
+
     void setTicksBeforeDamage(int ticksBeforeDamage);
+
     void damage(Player player);
+
     int getTicksToMove();
+
     int getTicksToShrink();
     boolean isInStorm(Entity entity);
 }
