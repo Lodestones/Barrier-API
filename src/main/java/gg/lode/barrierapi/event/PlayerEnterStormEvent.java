82d804045788bff6910840dad47f64d7da82ade2
John Aquino <41652412+apollyon600@users.noreply.github.com>
2025-07-30 03:58:49 -0700

    feat(api): added World to determine where the storm belongs to.

--- src/main/java/gg/lode/barrierapi/event/PlayerEnterStormEvent.java
+++ src/main/java/gg/lode/barrierapi/event/PlayerEnterStormEvent.java
@@ -1,26 +1,33 @@
 package gg.lode.barrierapi.event;
 
 import gg.lode.bookshelfapi.api.event.BaseEvent;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 public class PlayerEnterStormEvent extends BaseEvent {
+
+
+    private final World world;
     private final Player player;
     private int ticksBeforeDamage;
     private final boolean isInvulnerable;
 
-    public PlayerEnterStormEvent(Player player) {
+    public PlayerEnterStormEvent(World world, Player player) {
         this.player = player;
+        this.world = world;
         this.ticksBeforeDamage = 0;
         this.isInvulnerable = false;
     }
 
-    public PlayerEnterStormEvent(Player player, int ticksBeforeDamage) {
+    public PlayerEnterStormEvent(World world, Player player, int ticksBeforeDamage) {
         this.player = player;
+        this.world = world;
         this.ticksBeforeDamage = ticksBeforeDamage;
         this.isInvulnerable = false;
     }
 
-    public PlayerEnterStormEvent(Player player, int ticksBeforeDamage, boolean isInvulnerable) {
+    public PlayerEnterStormEvent(World world, Player player, int ticksBeforeDamage, boolean isInvulnerable) {
+        this.world = world;
         this.player = player;
         this.ticksBeforeDamage = ticksBeforeDamage;
         this.isInvulnerable = isInvulnerable;
--- src/main/java/gg/lode/barrierapi/event/PlayerLeaveStormEvent.java
+++ src/main/java/gg/lode/barrierapi/event/PlayerLeaveStormEvent.java
@@ -1,12 +1,15 @@
 package gg.lode.barrierapi.event;
 
 import gg.lode.bookshelfapi.api.event.BaseEvent;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 public class PlayerLeaveStormEvent extends BaseEvent {
     private final Player player;
+    private final World world;
 
-    public PlayerLeaveStormEvent(Player player) {
+    public PlayerLeaveStormEvent(World world, Player player) {
+        this.world = world;
         this.player = player;
     }
 
